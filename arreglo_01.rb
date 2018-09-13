2d7514e855ab2b378fbf995bf340e6d0e4f3d103
Ivan Leobardo Estrada Salinas <src>
2018-09-13 16:04:35 +0000

    Primer Push

---
+++ arreglo_01.rb
@@ -0,0 +1,10 @@
+arr = ["uno","dos","tres"]
+arr.each do |valor|
+puts valor
+end
+
+arr.each do |valor|
+puts "El Valor es: #{valor}"
+end
+
+arr.each {|valor| puts "El valor es: #{valor}"}
---
+++ est_if_01.rb
@@ -0,0 +1,9 @@
+today = Time.now
+
+if today.wednesday?
+puts "Salimos Temprano"
+elsif today.sunday?
+puts "Vamos a la Playa"
+else
+puts "Resolver programas de CodeSignal"
+end
