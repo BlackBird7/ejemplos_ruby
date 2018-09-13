cba8889db46832e81f4b10b0c08644f88a3c8b19
Ivan Leobardo Estrada Salinas <src>
2018-09-13 16:42:27 +0000

    Cambios Dia 2

---
+++ bloques_01.rb
@@ -0,0 +1,10 @@
+arreglo = (1..100)
+
+arreglo.each do |numero| 
+    if numero.even?
+    puts "El Numero #{numero} es Par"
+    else 
+    puts "El Numero #{numero} es Inpar"
+    end
+    end
+    
---
+++ est_while_01.rb
@@ -0,0 +1,7 @@
+inicio = 1
+termino = 10
+
+while inicio < termino
+    puts inicio
+    inicio = inicio + 1 #Inicio +=1
+    end
